f14e85b69898924f8c282d394ac28160689a11c2
Fraden1 <99405611+fraden1@users.noreply.github.com>
2025-05-31 18:28:36 +0200

    Add files via upload

--- ServiceChecker OriginalFile.bat
+++ ServiceChecker OriginalFile.bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCuDJGmW+0UiKRZHfze+D26oELwf7cvz6umMp18rXfE2d4HnzL2dKe8dpEznevY=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSjk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+IeA==
-::cxY6rQJ7JhzQF1fEqQJhZksaHErSXA==
-::ZQ05rAF9IBncCkqN+0xwdVsFAlTMbCXqZg==
-::ZQ05rAF9IAHYFVzEqQIDLQlCRQeHfEiyA7YX7fqb
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWG+N50s/JltnTxaHOWWJDrQO7Yg=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATEwksjIRRaDDeBLm6/CIYU6fr+jw==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCuDJGmW+0UiKRZHfze+D26oELwf7cvz6umMp18rXfE2d4Hn6bGcNukQ72bscJUm02gUndMJbA==
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
 cls
 
